9ca26939fc783ceac3e8f648479d3ab5aefee662
IceStormNG <carstenbraun@live.de>
2023-07-20 10:43:29 +0200

    Imporved detection whether to put laptop to sleep when display gets disconnected during clamshell mode.

--- app/Helpers/ClamshellModeControl.cs
+++ app/Helpers/ClamshellModeControl.cs
@@ -1,52 +1,80 @@
 ﻿using GHelper.Display;
 using GHelper.Mode;
 using Microsoft.Win32;
+using System.Management;
+using System.Diagnostics;
 
 namespace GHelper.Helpers
 {
     internal class ClamshellModeControl
     {
+        private int wmiMonitorCount = 0;
+        private int externalMonitorCount = 0;
+        private int internalMonitorCount = 0;
+
+
         public bool IsExternalDisplayConnected()
         {
             var devices = ScreenInterrogatory.GetAllDevices().ToArray();
 
             string internalName = AppConfig.GetString("internal_display");
-
+            externalMonitorCount = 0;
             foreach (var device in devices)
             {
                 if (device.outputTechnology != ScreenInterrogatory.DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY.DISPLAYCONFIG_OUTPUT_TECHNOLOGY_INTERNAL &&
                     device.outputTechnology != ScreenInterrogatory.DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY.DISPLAYCONFIG_OUTPUT_TECHNOLOGY_DISPLAYPORT_EMBEDDED
                     && device.monitorFriendlyDeviceName != internalName)
                 {
-                    Logger.WriteLine("Found external screen: " + device.monitorFriendlyDeviceName + ":" + device.outputTechnology.ToString());
-
-                    //Already found one, we do not have to check whether there are more
-                    return true;
+                    Logger.WriteLine("ClamshellModeControl: Found external screen: " + device.monitorFriendlyDeviceName + ":" + device.outputTechnology.ToString());
+                    ++externalMonitorCount;
                 }
 
             }
 
-            return false;
+            return externalMonitorCount > 0;
+        }
+
+
+        public void GetAllMonitorsWMI()
+        {
+            wmiMonitorCount = 0;
+            try
+            {
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_DesktopMonitor");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    Logger.WriteLine("ClamshellModeControl: Found WMI Monitor: " + queryObj["PNPDeviceID"].ToString());
+                    if (queryObj["PNPDeviceID"].ToString() is not null && queryObj["PNPDeviceID"].ToString() != String.Empty)
+                    {
+                        ++wmiMonitorCount;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                Logger.WriteLine(e.Message);
+            }
         }
 
         public bool IsInternalDisplayConnected()
         {
-            var devices = ScreenInterrogatory.GetAllDevices2().ToArray();
-
+            var devices = ScreenInterrogatory.GetAllDevices().ToArray();
+            internalMonitorCount = 0;
             foreach (var device in devices)
             {
                 if (device.outputTechnology == ScreenInterrogatory.DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY.DISPLAYCONFIG_OUTPUT_TECHNOLOGY_INTERNAL ||
                     device.outputTechnology == ScreenInterrogatory.DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY.DISPLAYCONFIG_OUTPUT_TECHNOLOGY_DISPLAYPORT_EMBEDDED)
                 {
-                    Logger.WriteLine("Found internal screen: " + device.monitorFriendlyDeviceName + ":" + device.outputTechnology.ToString());
-
-                    //Already found one, we do not have to check whether there are more
-                    return true;
+                    Logger.WriteLine("ClamshellModeControl: Found internal screen: " + device.monitorFriendlyDeviceName + ":" + device.outputTechnology.ToString());
+                    ++internalMonitorCount;
                 }
 
             }
 
-            return false;
+            return internalMonitorCount > 0;
         }
 
         public bool IsLidClosed()
@@ -54,14 +82,70 @@ namespace GHelper.Helpers
             return !IsInternalDisplayConnected();
         }
 
+        public void SubscribeToDisplayConnectionEvents()
+        {
+            try
+            {
+                ManagementEventWatcher watcher = new ManagementEventWatcher();
+                watcher.EventArrived += new EventArrivedEventHandler(EventArrived);
+                watcher.Scope = new ManagementScope("root\\wmi");
+                watcher.Query = new WqlEventQuery("SELECT * FROM AsusAtkWmiEvent WHERE EventID = 192");
+                watcher.Start();
+            }
+            catch
+            {
+                Logger.WriteLine("ClamshellModeControl: Can't connect to ASUS WMI events");
+            }
+        }
+
+        private void EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            if (e.NewEvent is null) return;
+            int EventID = int.Parse(e.NewEvent["EventID"].ToString());
+            if (EventID == 192)
+            {
+                int oldCount = wmiMonitorCount;
+                GetAllMonitorsWMI();
+                int displayDelta = Math.Abs(oldCount - wmiMonitorCount);
+
+                if (oldCount > wmiMonitorCount)
+                {
+                    Logger.WriteLine("ClamshellModeControl: Display disconnected. Displays removed: " + displayDelta);
+                }
+                else
+                {
+                    Logger.WriteLine("ClamshellModeControl: Display connected. New Displays: " + displayDelta);
+                }
+
+
+                bool externalDisplayLeft = true;
+
+                if (oldCount < wmiMonitorCount && displayDelta >= externalMonitorCount)
+                {
+                    //All external screens got disconnected
+                    externalDisplayLeft = false;
+                }
+
+                //Only call if there was an external screen connected, clamshall is engegaged, but all external screens are (likely) gone now
+                if (IsExternalDisplayConnected() && IsClamshellEnabled() && !IsInternalDisplayConnected() && !externalDisplayLeft)
+                {
+                    //PowerNative.SetLidAction(1, true);
+                    Process.Start("C:\\Windows\\System32\\DisplaySwitch.exe", "/internal");
+                    Task.Delay(1000 * 1).ContinueWith(t => Program.acpi.DeviceSet(AsusACPI.UniversalControl, AsusACPI.KB_Sleep, "Sleep"));
+
+                }
+            }
+        }
+
 
         public void PrintClamshellStatus()
         {
-            Logger.WriteLine("Clamshell: " + IsClamshellEnabled() + ", External Screen: " + 
-                IsExternalDisplayConnected() + ", Internal Screen: " + 
-                IsInternalDisplayConnected() + ", Lid Closed: " + IsLidClosed());
+            Logger.WriteLine("ClamshellModeControl: " + IsClamshellEnabled() + ", Total Screens: " +
+                wmiMonitorCount + ", External Screens: " +
+                externalMonitorCount + ", Internal Screens: " +
+                internalMonitorCount + ", Lid Closed: " + IsLidClosed());
         }
-    
+
 
         public bool IsClamshellEnabled()
         {
@@ -84,13 +168,12 @@ namespace GHelper.Helpers
             if (IsInClamshellMode() && IsClamshellEnabled())
             {
                 PowerNative.SetLidAction(0, true);
-                Logger.WriteLine("Engaging Clamshell Mode");
+                Logger.WriteLine("ClamshellModeControl: Engaging Clamshell Mode");
             }
             else
             {
                 PowerNative.SetLidAction(1, true);
-                Logger.WriteLine("Disengaging Clamshell Mode");
-
+                Logger.WriteLine("ClamshellModeControl: Disengaging Clamshell Mode");
             }
         }
 
@@ -102,11 +185,13 @@ namespace GHelper.Helpers
         public void RegisterDisplayEvents()
         {
             SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+            SubscribeToDisplayConnectionEvents();
+            GetAllMonitorsWMI();
         }
 
         private void SystemEvents_DisplaySettingsChanged(object? sender, EventArgs e)
         {
-            Logger.WriteLine("Display configuration changed.");
+            Logger.WriteLine("ClamshellModeControl: Display configuration changed.");
 
             if (IsClamshellEnabled())
                 ToggleLidAction();
